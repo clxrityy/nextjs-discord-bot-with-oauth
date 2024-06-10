d6095d8e1eebd6d79d8acb166a1074a3a775f373
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-06-09 23:40:43 -0400

    oauth2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,64 @@
 import { Suspense } from "react"
 import { GlobalCommands } from "./global-commands"
+import { parseUser } from "@/utils/user"
+import { redirect } from "next/navigation";
+import { CONFIG } from "@/config";
 
 export default async function Page() {
-  return (
-    <main className="container mx-auto px-3 py-6">
-      <section className="grid grid-cols-1 gap-2">
-        <h1 className="text-4xl font-bold tracking-tight lg:text-5xl">NextBot</h1>
-        <h2 className="text-xl tracking-tight text-slate-500">
-          A Discord bot template built with Next.js that runs in the edge runtime.
-        </h2>
-        <div className="flex gap-2">
-          <a
-            className="ring-offset-background focus-visible:ring-ring inline-flex h-10 w-fit items-center justify-center rounded-md bg-[#7289DA] px-4 py-2 text-sm font-medium text-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2"
-            href="https://discord.gg/NmXuqGgkb3"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Try it out
-          </a>
-          <a
-            className="ring-offset-background focus-visible:ring-ring inline-flex h-10 w-fit items-center justify-center rounded-md bg-slate-900 px-4 py-2 text-sm font-medium text-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2"
-            href="https://github.com/jzxhuang/nextjs-discord-bot"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Github
-          </a>
-        </div>
-      </section>
 
-      <section className="flex flex-col gap-2 pt-12">
-        <p className="font-semibold">
-          This is an example of an admin portal might look like. It leverages RSCs to fetch the Slash commands
-          associated with the Discord bot!
-        </p>
-        <Suspense fallback={null}>
-          <GlobalCommands />
-        </Suspense>
-      </section>
+  const user = parseUser();
+
+  if (!user) {
+    return redirect(CONFIG.OAUTH2_INVITE_URL);
+  }
+
+  return (
+    <main className="">
+      <div className="container mx-auto px-3 py-6">
+        <section className="grid grid-cols-1 gap-2">
+          <h1 className="text-4xl font-bold tracking-tight lg:text-5xl">NextBot</h1>
+          <h2 className="text-xl tracking-tight text-slate-500">
+            A Discord bot template built with Next.js that runs in the edge runtime.
+          </h2>
+          <div className="flex gap-2">
+            <a
+              className="ring-offset-background focus-visible:ring-ring inline-flex h-10 w-fit items-center justify-center rounded-md bg-[#7289DA] px-4 py-2 text-sm font-medium text-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2"
+              href="https://discord.gg/NmXuqGgkb3"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Try it out
+            </a>
+            <a
+              className="ring-offset-background focus-visible:ring-ring inline-flex h-10 w-fit items-center justify-center rounded-md bg-slate-900 px-4 py-2 text-sm font-medium text-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2"
+              href="https://github.com/jzxhuang/nextjs-discord-bot"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </div>
+        </section>
+
+        <section className="flex flex-col gap-2 pt-12">
+          <p className="font-semibold">
+            This is an example of an admin portal might look like. It leverages RSCs to fetch the Slash commands
+            associated with the Discord bot!
+          </p>
+          <Suspense fallback={null}>
+            <GlobalCommands />
+          </Suspense>
+        </section>
+
+        <div className="w-fit px-4 py-2 space-y-2">
+          <h2 className="text-2xl font-semibold">
+            Welcome, <span className="font-bold tracking-wider text-zinc-600">{user.username}</span>!
+          </h2>
+          <pre className="bg-zinc-100 rounded-md p-2">
+            <code>{JSON.stringify(user, null, 2)}</code>
+          </pre>
+        </div>
+      </div>
     </main>
   )
 }
